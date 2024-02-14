68d5f1ec64b2e7255e2612c5a02aaa4e787475e6
Daniel Connelly <dhconnelly@gmail.com>
2024-02-14 12:05:11 +0100

    implement backprop

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ fn main() {
     let w1 = Value::of(-3.0, "w1");
     let w2 = Value::of(1.0, "w2");
     let b = Value::of(6.881373587019543, "b");
-    let x1w1 = x1 * w1;
-    let x2w2 = x2 * w2;
-    let x1w1x2w2 = x1w1 + x2w2;
-    let n = x1w1x2w2 + b;
-    let o = n.tanh();
+    let x1w1 = x1.mul(&w1);
+    let x2w2 = x2.mul(&w2);
+    let x1w1x2w2 = x1w1.add(&x2w2);
+    let n = x1w1x2w2.add(&b);
+    let mut o = n.tanh();
+    o.backward();
     trace(&o);
 }
